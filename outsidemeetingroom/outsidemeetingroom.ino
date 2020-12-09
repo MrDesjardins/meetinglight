e105ef3b96d54044bd0cc5db2d7ec0953d59614c
Patrick Desjardins <pdesjardins3@gatech.edu>
2020-12-08 21:49:25 -0800

    Working

--- outsidemeetingroom/outsidemeetingroom.ino
+++ outsidemeetingroom/outsidemeetingroom.ino
@@ -3,12 +3,14 @@
 // 1.2) Under tool, select the ESP8226 and select the Lolin(wesmo) D1, R2 & Pro
 // 2) Tools>Library management
 #include <ESP8266WiFi.h>
-// Expect to have this machine set to 10.0.0.231
+#include "libs/ESP8266Ping.h"
+// Expect to have this machine set to 10.0.0.234
 const char* ssid     = "CanadaWifi_2_4";      // SSID of local network
 const char* password = "5142931290";                // Password on network
 byte mac[6];
-int switch_pin = D1;
+int relay_pin = D1;
 WiFiClientSecure client; //For ESP8266 boards
+const IPAddress remote_ip(10, 0, 0, 231);
 
 void setup() {
   Serial.println("Setup");
@@ -19,6 +21,7 @@ void setup() {
     Serial.print("."); delay(500);
   }
   Serial.println("Connected: ");
+  
   Serial.println(WiFi.localIP());
 
   WiFi.macAddress(mac);
@@ -34,39 +37,35 @@ void setup() {
   Serial.print(mac[4],HEX);
   Serial.print(":");
   Serial.println(mac[5],HEX);
-
+  
   client.setInsecure();
-  pinMode(switch_pin, INPUT_PULLUP);
+  pinMode(relay_pin, OUTPUT);
 }
 
 void loop() {
-  int buttonState = digitalRead(switch_pin);
-  Serial.println(buttonState);
-  if (buttonState == 0){ // 0 == On == Meeting 
-    Serial.println("Pin state is 0, meeting mode is enabled");
-    if (WiFi.status() != WL_CONNECTED){
-      Serial.println("Meeting means we need wifi: connecting WiFi from loop");
-      WiFi.begin(ssid, password);
-       while (WiFi.status() != WL_CONNECTED) {
-        Serial.print("."); delay(500);
-      }
-      Serial.println("Meeting on: connected from loop with ip ");
-      Serial.println(WiFi.localIP());
-    }
-  }
-  else {
-    // 1 == Off == No meeting close wifi
-    Serial.println("Pin state is 1, meeting mode is disabled");
-    if (WiFi.status() == WL_CONNECTED){
-      stopWiFiAndSleep();    
-    }
-  }
-  delay(100);  
-}
+  // https://www.arduino.cc/en/Reference/WiFi101Ping
+  // Ping check. If ping then in a meeting
 
-void stopWiFiAndSleep() {
-  WiFi.disconnect();
-  WiFi.mode(WIFI_OFF);
-  WiFi.forceSleepBegin();
-  delay(1);
+
+  Serial.print("Pinging ");
+  Serial.print(remote_ip);
+  Serial.print(": ");
+
+  if (Ping.ping(remote_ip)) {
+    Serial.println("SUCCESS!");
+    digitalWrite(relay_pin,HIGH);
+  } else {
+    Serial.println("FAILED!");
+    digitalWrite(relay_pin,LOW);
+  }
+
+  delay(100);
+
+  // Test:
+    // digitalWrite(relay_pin,HIGH);
+    // Serial.println("ON");
+    // delay(1000);
+    // digitalWrite(relay_pin,LOW);
+    // Serial.print("-OFF");
+    // delay(1000);
 }
